5c41f92618d14bc4966de77af6139c90f30c9d2d
DavideDiPietro <davide.dipietro3@gmail.com>
2022-05-30 16:20:18 +0200

    Migrate - tabella beers

---
+++ database/migrations/2022_05_30_141120_create_beers_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBeersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('beers', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome');
+            $table->string('colore');
+            $table->decimal('tasso_alcolico', 3, 1);
+            $table->decimal('prezzo', 8, 2);
+            $table->string('descrizione');
+            $table->string('codice_prodotto');
+            $table->boolean('disponibile');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('beers');
+    }
+}
